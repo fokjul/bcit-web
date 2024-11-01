bcc1024b9e32b60cebc42b29371e8d5b8ac9c5df
fokjul <julia.fok86@gmail.com>
2024-11-01 10:20:08 -0700

    fixes mobile view for offerings

--- client/src/components/Atoms/ScheduleInfo/ScheduleInfo.jsx
+++ client/src/components/Atoms/ScheduleInfo/ScheduleInfo.jsx
@@ -6,7 +6,8 @@ import './ScheduleInfo.scss';
 const ScheduleInfo = ({ meetingMetrics }) => {
 
   return (
-    <div className='meetingMetrics'>
+    <>
+      <div className='meetingMetrics'>
       <div className='meetingMetrics__header'>
         <div className='meetingMetrics__header-cell'>Dates</div>
         <div className='meetingMetrics__header-cell'>Days</div>
@@ -23,7 +24,32 @@ const ScheduleInfo = ({ meetingMetrics }) => {
         </div>
         
       ))}
-    </div>
+      </div>
+      <div className='meetingMetrics--mobile'>
+      {meetingMetrics?.map((metricItem, index) => (
+        <div className='meetingMetrics__row'>
+            <div className='meetingMetrics__metric'>
+              <div className='meetingMetrics__header-cell'>Dates</div>
+              <div key={index} className='meetingMetrics__cell'>{metricItem.dates}</div>
+            </div>
+            <div className='meetingMetrics__metric'>
+              <div className='meetingMetrics__header-cell'>Days</div>
+              <div key={index} className='meetingMetrics__cell'>{metricItem.days}</div>
+            </div>
+            <div className='meetingMetrics__metric'>
+              <div className='meetingMetrics__header-cell'>Time</div>
+              <div key={index} className='meetingMetrics__cell'>{metricItem.times}</div>
+            </div>
+            <div className='meetingMetrics__metric'>
+              <div className='meetingMetrics__header-cell'>Locations</div>
+              <div key={index} className='meetingMetrics__cell'>{metricItem.locations}</div>
+            </div>
+        </div>
+        
+      ))}
+      </div>
+    </>
+   
   );
 }
 
--- client/src/components/Atoms/StatusTag/StatusTag.jsx
+++ client/src/components/Atoms/StatusTag/StatusTag.jsx
@@ -5,18 +5,6 @@ import { check, stop, info} from '../../../assets/icons'
 const StatusTag = ({status}) => {
   console.log(status)
   return (
-    // <div className={`statusTag statusTag--${status.value}`}>
-    //     <div className='statusTag__img-container'>
-    //       <img 
-    //         src={
-    //           status.value === 'inProgress' ? info :
-    //           status.value === 'full' ? stop :
-    //           status.value === 'available' && check
-    //         } 
-    //         alt=''></img>
-    //     </div>
-    //     <p className={`statusTag-text statusTag-text--${status.value}`}>{status.name}</p>
-    // </div>
     <div className={`statusTag statusTag--${status.value}`}>
       <p className={`statusTag-text statusTag-text--${status.value}`}>{status.name}</p>
     </div>
