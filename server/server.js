037dac5bcd370c1536368ef5eba49afb39c2fd7d
fokjul <julia.fok86@gmail.com>
2024-11-05 21:38:56 -0800

    updates courseData file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
+// server.js
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
 // Load environment variables from .env file
 const app = express();
-const PORT = process.env.PORT;
 
 // Middleware to parse JSON bodies and handle CORS
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Import course routes
@@ -16,7 +16,5 @@ const courseRoutes = require("./routes/course-routes");
 // Mount API routes under /
 app.use("/courses", courseRoutes);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
