22fadae64f3d45ca662aa71d175c3a377b618e09
fokjul <40266249+fokjul@users.noreply.github.com>
2024-11-13 10:46:20 -0800

    updates tab cursor type and status tag design

--- client/src/components/Atoms/StatusTag/StatusTag.jsx
+++ client/src/components/Atoms/StatusTag/StatusTag.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './StatusTag.scss';
-import { check, stop, info} from '../../../assets/icons'
+import { check, stop, clock} from '../../../assets/icons'
 
 const StatusTag = ({status}) => {
   return (
+    
+    // <div className={`statusTag statusTag--${status.value}`}>
+    //   <p className={`statusTag-text statusTag-text--${status.value}`}>{status.name}</p>
+    // </div>
     <div className={`statusTag statusTag--${status.value}`}>
+      <div>
+      <img src={status.value === 'available' ? check : status.value === 'inProgress' ? clock : stop} alt='icon'/>
       <p className={`statusTag-text statusTag-text--${status.value}`}>{status.name}</p>
-    </div>
+      </div>
+    
+  </div>
   )
 }
 
